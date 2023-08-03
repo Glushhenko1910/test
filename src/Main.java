b6e96a23a1532d73cbbf104f422c0b54fe0715a9
HP <!@mail.ru>
2023-08-03 20:33:53 +0300

    First Commit

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+        if (a<b){
+            System.out.println("Еще не прозвин
+                    '';l.kjhhgff'[lur-md'
+
+        }else if (a==b){
+            System.out.println("Звоним");2
+
+
+        }else if (a>b){
+            System.out.println("Проспали");
+        }
+
+
+
+    }
+}
