81ad6a476e45e3a723dcca59d310f92730b3e149
HP <!@mail.ru>
2023-08-03 20:53:21 +0300

    Second Commit

--- src/Main.java
+++ src/Main.java
@@ -2,25 +2,15 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
-
         Scanner scanner = new Scanner(System.in);
         int a = scanner.nextInt();
         int b = scanner.nextInt();
-
-        if (a<b){
-            System.out.println("Еще не прозвин
-                    '';l.kjhhgff'[lur-md'
-
-        }else if (a==b){
-            System.out.println("Звоним");2
-
-
-        }else if (a>b){
+        if (a < b) {
+            System.out.println("Еще не позвоним");
+        } else if (a == b) {
+            System.out.println("Звоним");
+        } else if (a > b) {
             System.out.println("Проспали");
         }
-
-
-
     }
 }
